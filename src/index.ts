c8ede024fb36798337a7ef3ad7d071fad36a0ddd
nando-castro <fernando.castro10.master@gmail.com>
2022-08-31 16:32:36 -0300

    chore: router config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
+import router from "./routes";
 
 dotenv.config();
 const app = express();
-
+app.use(express.json());
 app.use(cors());
+app.use(router);
 
 const { PORT } = process.env;
 
--- src/repositories/companyRepository.ts
+++ src/repositories/companyRepository.ts
@@ -1,4 +1,4 @@
-import { connection } from "../database.js";
+import { connection } from "../databases/database";
 
 export interface Company {
   id: number;

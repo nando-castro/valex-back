6a06698194dc9d0d17d83c19337b5a50dbd992b5
nando-castro <fernando.castro10.master@gmail.com>
2022-09-03 19:48:00 -0300

    chore: schema validate middleware

---
+++ src/middlewares/schemaValidateMiddleware.ts
@@ -0,0 +1,11 @@
+import { ObjectSchema } from "joi";
+import { NextFunction, Request, Response } from "express";
+
+export function schemaValidateMiddleware(schema: ObjectSchema) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req, { abortEarly: false });
+    if (error)
+      return res.status(422).send(error.details.map(({ message }) => message));
+    next();
+  };
+}
